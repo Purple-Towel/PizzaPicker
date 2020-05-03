8cc367c2c6af964a70221f21b9192c8f6fd3fd83
Angad Grewal <angadgrewal31@gmail.com>
2020-05-03 20:38:17 +0000

    changed the DB schema for an order to now use a boolean for pickup status that the web app will set

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -5,6 +5,6 @@ CREATE TABLE orders (
   customer_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 
   ordered_at TIMESTAMP DEFAULT NOW(), /*might need to remove DEFAULT NOW() */
-  pickup_time SMALLINT NOT NULL /* arbitrary time (30 minutes, etc.) */
+  ready_for_pickup BOOLEAN NOT NULL DEFAULT FALSE /* now a boolean that is false when an order is created, our web app will update the pickup status with a timer */
 );
 
