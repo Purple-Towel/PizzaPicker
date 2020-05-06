37baae9f5d856831756b1c54b62ed50be9c5a316
Calvin Zheng <calvinzheng08@hotmail.com>
2020-05-06 01:47:51 +0000

    added twilio SMS  functions for Client and Restaurant

---
+++ twilio/smstoRestaurant.js
@@ -0,0 +1,20 @@
+require('dotenv').config({silent: true})
+const accountSid = process.env.TWILIO_ACCOUNT_SID2;
+const authToken = process.env.TWILIO_AUTH_TOKEN2;
+const fromPhone = process.env.TWILIO_NUMBER2;
+const calvin = process.env.Calvin
+//twilio account and number in env
+const client = require('twilio')(accountSid, authToken);
+
+//function called on click
+function sendmessagetoRestaurant() {
+  return client.messages
+    .create({
+    body: `TO RESTAURANT: Order has been placed`,
+    from: fromPhone,
+    to: calvin
+  })
+}
+
+sendmessagetoRestaurant()
+module.exports = { sendmessagetoRestaurant };
