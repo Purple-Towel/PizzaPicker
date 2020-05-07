c5c4103883733f6d8491b87c3150d9d1045a5fa7
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-05-07 20:02:03 +0000

    try to add orderInMemory to orderlines table in db, so far unsuccessful

--- routes/cart.js
+++ routes/cart.js
@@ -9,12 +9,24 @@ module.exports = (db) => {
     res.render('cart', { userID });
   });
 
+  router.post("/", (req, res) => {
+    const totalOrder = req.body.orderInMemory;
+
+    for (let line of totalOrder) {
+      db.query(`
+        INSERT INTO order_lines (order_id, dish_id, quantity)
+        VALUES (1, $1, $2, $3, $4);
+      `, [line[0].ID, line[1]])
+      .then(res => console.log(res.rows));
+    }
+  });
+
   router.post('/smstoRestaurant', (req, res) => {
     smstoRestaurant()
   });
 
-  router.post('/smstoClient', (req,res) => {
-    setTimeout(() =>smstoClient(), 10000)
+  router.post('/smstoClient', (req, res) => {
+    setTimeout(() => smstoClient(), 10000)
   });
 
   return router;
