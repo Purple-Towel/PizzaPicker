cd4da3ba283cfd8e267cc09d443c408608c3c45c
Angad Grewal <angadgrewal31@gmail.com>
2020-05-08 01:43:00 +0000

    linted twilio directory js files

--- twilio/smstoClient.js
+++ twilio/smstoClient.js
@@ -1,33 +1,33 @@
-require('dotenv').config({silent: true})
+require('dotenv').config({silent: true});
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const fromPhone = process.env.TWILIO_NUMBER;
-const calvin = process.env.Calvin
+const calvin = process.env.Calvin;
 //twilio account and number in env
 const client = require('twilio')(accountSid, authToken);
 
 //function called on timeout delay
-function smstoClient() {
+const smstoClient = function() {
   return client.messages
     .create({
-    body: `Hi ${calvin},
-    Thanks for ordering with Pizza Planet.
-    Your order is ready for pickup`,
-    from: fromPhone,
-    to: calvin
-  })
-}
+      body: `Hi ${calvin},
+      Thanks for ordering with Pizza Planet.
+      Your order is ready for pickup`,
+      from: fromPhone,
+      to: calvin
+    });
+};
 
-function smstoclientInitial(data) {
+const smstoclientInitial = function(data) {
   return client.messages
     .create({
-    body: `Hi ${calvin},
-    Thanks for ordering with Pizza Planet.
-    We have recieved your order and it will be completed in ${data} minutes.`,
-    from: fromPhone,
-    to: calvin
-  })
-}
+      body: `Hi ${calvin},
+      Thanks for ordering with Pizza Planet.
+      We have recieved your order and it will be completed in ${data} minutes.`,
+      from: fromPhone,
+      to: calvin
+    });
+};
 
 
 module.exports = { smstoClient, smstoclientInitial };
--- twilio/smstoRestaurant.js
+++ twilio/smstoRestaurant.js
@@ -1,19 +1,19 @@
-require('dotenv').config({silent: true})
+require('dotenv').config({silent: true});
 const accountSid = process.env.TWILIO_ACCOUNT_SID2;
 const authToken = process.env.TWILIO_AUTH_TOKEN2;
 const fromPhone = process.env.TWILIO_NUMBER2;
-const calvin = process.env.Calvin
+const calvin = process.env.Calvin;
 //twilio account and number in env
 const client = require('twilio')(accountSid, authToken);
 
 //function called on click
-function smstoRestaurant() {
+const smstoRestaurant = function() {
   return client.messages
     .create({
-    body: `TO RESTAURANT: Order has been placed`,
-    from: fromPhone,
-    to: calvin
-  })
-}
+      body: `TO RESTAURANT: Order has been placed`,
+      from: fromPhone,
+      to: calvin
+    });
+};
 
 module.exports = { smstoRestaurant };
